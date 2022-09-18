54b4325e2c599e114ce228033e92e8061994a104
Shahadat Hossain <60577101+shahadathossain4536@users.noreply.github.com>
2022-09-18 08:46:44 +0600

    done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Login />}></Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="cart" element={<Cart />}></Route>
       </Routes>
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,6 +6,7 @@ import cartImg3 from "../../src/img/download (1).jpg";
 import {
   IoAddCircleOutline,
   IoCameraOutline,
+  IoRemoveCircleOutline,
   IoSearchOutline,
 } from "react-icons/io5";
 import { FaRupeeSign } from "react-icons/fa";
@@ -36,53 +37,177 @@ const cart = () => {
   return (
     <section
       // style={{ background: `url(${img})`, backgroundSize: "cover" }}
-      className="flex justify-center items-center"
+      className="flex justify-center items-center py-4"
     >
       <div className="w-[375px] ">
-        <div class="flex items-center  py-2 px-3  mb-4 border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
-          <IoSearchOutline className="w-9 h-9 " />
-          <input
-            class="pl-2 rounded-none w-full max-w-xs text-black bg-transparent outline-none"
-            type="text"
-            placeholder="Search"
-          />
+        <div className="border-b-2 pb-5">
+          <div class="flex items-center  py-2 px-3  mb-4 border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
+            <IoSearchOutline className="w-9 h-9 " />
+            <input
+              class="pl-2 rounded-none w-full max-w-xs text-black bg-transparent outline-none"
+              type="text"
+              placeholder="Search"
+            />
 
-          <IoCameraOutline className="w-9 h-9 border-black pl-2 border-l-2" />
-        </div>
-
-        <div>
-          <div className="flex justify-around items-start">
-            <img src={cartImg1} alt="" srcset="" />
-            <div>
-              <h1 className="text-xl font-bold">Anchor Bracelet</h1>
-              <p className="flex items-center">
-                {" "}
-                <sup>
-                  <FaRupeeSign />
-                </sup>{" "}
-                <span className="text-xl"> 12,000</span>
-              </p>
-              <p>Color:Blue</p>
-              <p className="text-green-500">In stock</p>
-            </div>
+            <IoCameraOutline className="w-9 h-9 border-black pl-2 border-l-2" />
           </div>
-          <div className="flex justify-evenly items-center">
-            <div className="flex justify-center items-center w-28  py-2 px-3  mb-4 border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
-              <IoAddCircleOutline className="w-[20px]" />
-              <input
-                className="pl-2 rounded-none w-16  text-black bg-transparent outline-none"
-                type="text"
-                name=""
-                id=""
+
+          <div>
+            <div className="flex justify-around items-start">
+              <img
+                className="w-[100px] h-[100px]"
+                src={cartImg1}
+                alt=""
+                srcset=""
               />
-              <IoAddCircleOutline className="w-[20px]" />
+              <div>
+                <h1 className="text-xl font-bold">Querty Watch</h1>
+                <p className="flex items-center">
+                  {" "}
+                  <sup>
+                    <FaRupeeSign />
+                  </sup>{" "}
+                  <span className="text-xl font-semibold"> 12,000</span>
+                </p>
+                <p className="text-slate-500 text-lg">
+                  Color:
+                  <span>Blue</span>
+                </p>
+                <p className="text-green-500">In stock</p>
+              </div>
             </div>
-            <div className="flex justify-center items-center gap-2">
-              <button className="btn btn-sm">Delete</button>
-              <button className="btn btn-sm">See more like this</button>
+            <div className="flex justify-evenly items-center">
+              <div className="flex justify-center items-center w-28  py-2  border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
+                <button>
+                  <IoAddCircleOutline className="w-[40px]" />
+                </button>
+                <input
+                  className="pl-2 rounded-none w-full  text-black bg-transparent outline-none text-center"
+                  type="text"
+                  name=""
+                  id=""
+                />
+                <button>
+                  <IoRemoveCircleOutline className="w-[40px]" />
+                </button>
+              </div>
+              <div className="flex justify-center items-center gap-2 ml-1">
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 text-orange-500">
+                  Delete
+                </button>
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 bg-orange-500 text-white">
+                  See more like this
+                </button>
+              </div>
             </div>
           </div>
         </div>
+        {/* 222222222222222222222 */}
+        <div className="border-b-2 pb-5">
+          <div>
+            <div className="flex justify-around items-start">
+              <img
+                className="w-[100px] h-[100px]"
+                src={cartImg2}
+                alt=""
+                srcset=""
+              />
+              <div>
+                <h1 className="text-xl font-bold">Yoga mat</h1>
+                <p className="flex items-center">
+                  {" "}
+                  <sup>
+                    <FaRupeeSign />
+                  </sup>{" "}
+                  <span className="text-xl font-semibold"> 1,000</span>
+                </p>
+                <p className="text-slate-500 text-lg">
+                  Color:
+                  <span>Blue</span>
+                </p>
+                <p className="text-green-500">In stock</p>
+              </div>
+            </div>
+            <div className="flex justify-evenly items-center">
+              <div className="flex justify-center items-center w-28  py-2  border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
+                <button>
+                  <IoAddCircleOutline className="w-[40px]" />
+                </button>
+                <input
+                  className="pl-2 rounded-none w-full  text-black bg-transparent outline-none text-center"
+                  type="text"
+                  name=""
+                  id=""
+                />
+                <button>
+                  <IoRemoveCircleOutline className="w-[40px]" />
+                </button>
+              </div>
+              <div className="flex justify-center items-center gap-2 ml-1">
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 text-orange-500">
+                  Delete
+                </button>
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 bg-orange-500 text-white">
+                  See more like this
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 222222222222222222222 */}
+        {/* 333333333333333333333 */}
+        <div className="border-b-2 pb-5">
+          <div>
+            <div className="flex justify-around items-start">
+              <img
+                className="w-[100px] h-[100px]"
+                src={cartImg3}
+                alt=""
+                srcset=""
+              />
+              <div>
+                <h1 className="text-xl font-bold">Yoga mat</h1>
+                <p className="flex items-center">
+                  {" "}
+                  <sup>
+                    <FaRupeeSign />
+                  </sup>{" "}
+                  <span className="text-xl font-semibold"> 1,000</span>
+                </p>
+                <p className="text-slate-500 text-lg">
+                  Color:
+                  <span>Blue</span>
+                </p>
+                <p className="text-green-500">In stock</p>
+              </div>
+            </div>
+            <div className="flex justify-evenly items-center">
+              <div className="flex justify-center items-center w-28  py-2  border-0 rounded-2xl bg-gray-400 bg-opacity-20 border-slate-800 ">
+                <button>
+                  <IoAddCircleOutline className="w-[40px]" />
+                </button>
+                <input
+                  className="pl-2 rounded-none w-full  text-black bg-transparent outline-none text-center"
+                  type="text"
+                  name=""
+                  id=""
+                />
+                <button>
+                  <IoRemoveCircleOutline className="w-[40px]" />
+                </button>
+              </div>
+              <div className="flex justify-center items-center gap-2 ml-1">
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 text-orange-500">
+                  Delete
+                </button>
+                <button className="py-1 px-2 rounded-full border-4 border-orange-500 bg-orange-500 text-white">
+                  See more like this
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* 333333333333333333333333333 */}
       </div>
     </section>
   );
